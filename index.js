625234b62a24dadb536f4949be5fd944a8d0892e
Puja Podder <46476218+pujapodder@users.noreply.github.com>
2019-06-26 03:04:07 -0500

    Add animation

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-let images=["images/1.jpg",
-            "images/2.jpg",
-            "images/3.jpg",
-            "images/4.jpg",
-            "images/5.jpg"];
-let i =0;
+let images = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg"
+];
+let i = 0;
 
-function slides(){
-document.getElementById("slider").src=images[i];
-if(i<(images.length-1))
-{
+function slides() {
+  document.getElementById("slider").src = images[i];
+  if (i < images.length - 1) {
     i++;
-}
-else{
-    i=0;
-}
+  } else {
+    i = 0;
+  }
 }
 
-setInterval(slides,2000);
+setInterval(slides, 2000);
+function RecipeButtonFunction_ChickenTikka() {
+  alert("Recipe of Chicken Tikka");
+  document.getElementById("recipe").style.display = "flex";
+  document.getElementById("IngredientList").style.textAlign = "left";
+}
 
-function RecipeButtonFunction(){
-   alert("Button Clicked");
-    console.log("Button Clicked");
+function RecipeButtonFunction() {
+  alert("Recipe of");
+  document.getElementById("recipe").style.display = "visible";
+}
+
+window.onload = function() {
+  document.getElementById("recipe").style.display = "none";
+};
+
+function slideshow() {
+  var Bar = document.getElementById("checkBar");
+  if (Bar.display === "none") {
+    Bar.display = "block";
+  } else {
+    Bar.display = "none";
+  }
 }
